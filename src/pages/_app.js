0da5259de632fa2bb22ec19f8e356896433900ce
alireza_bisout <alirezabisout1145@gmail.com>
2025-03-26 00:05:47 +0330

    added home page (HTML)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
 
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
